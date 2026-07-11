a2263d8850e020262678bc54b4dfef7d45cdccf6
Druhin Sourya Datta Bommisetti <f20240182@hyderabad.bits-pilani.ac.in>
2026-07-11 13:55:28 +0530

    Add: max-consecutive-ones - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int curr_len = 0;
+        int max_len = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i]== 1) curr_len++ ;
+            else if(nums[i]== 0){
+                max_len = max(curr_len,max_len);
+                curr_len = 0;
+            }
+        }
+        max_len = max(max_len,curr_len);
+        return max_len;
+    }
+};
