eafae0a119a11f4cf34d91d24e82f9737fdb42c5
Druhin Sourya Datta Bommisetti <f20240182@hyderabad.bits-pilani.ac.in>
2026-07-20 18:01:13 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int red = 0;
+        for(int i = 0; i<n; i++){
+            if(nums[i]!=val){
+                nums[red] = nums[i];
+                red++;
+            }
+        }
+        return (red);
+    }
+};
